ba380adc92d890f26d076085a7fba3e28933f307
Federico279 <felu1816@gmail.com>
2025-01-10 15:40:00 +0100

    improved look

--- scripts/covid.js
+++ scripts/covid.js
@@ -41,8 +41,10 @@ Promise.all([
     const width = 960, height = 600;
     const svg = d3.select("#map")
         .append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        .attr("preserveAspectRatio", "xMidYMid meet");
 
     const projection = d3.geoEqualEarth()
         .fitSize([width, height], geojson);
@@ -54,8 +56,9 @@ Promise.all([
         .style("position", "absolute")
         .style("background", "rgba(0, 0, 0, 0.8)")
         .style("color", "white")
-        .style("padding", "5px 10px")
+        .style("padding", "10px")
         .style("border-radius", "5px")
+        .style("box-shadow", "0 0 10px rgba(0, 0, 0, 0.5)")
         .style("pointer-events", "none")
         .style("opacity", 0);
 
@@ -69,7 +72,7 @@ Promise.all([
             const value = +latestData[country]?.["Total confirmed deaths due to COVID-19 per 100,000 people"];
             return isNaN(value) || value <= 0 ? "#ccc" : colorScale(value);
         })
-        .attr("stroke", "#333")
+        .attr("stroke", "#999")
         .attr("stroke-width", 0.5)
         .on("mouseover", (event, d) => {
             const country = d.properties.name;
@@ -92,46 +95,45 @@ Promise.all([
             tooltip.style("opacity", 0);
         });
 
-        const legend = svg.append("g")
+    const legend = svg.append("g")
         .attr("transform", `translate(20, ${height - 150})`);
-    
+
     const legendScale = d3.scaleLinear()
         .domain(colorScale.domain())
         .range([0, 100]);
-    
+
     const legendAxis = d3.axisRight(legendScale)
         .ticks(5);
-    
-    const legendBarWidth = 30; 
-    
+
+    const legendBarWidth = 30;
+
     legend.selectAll("rect")
         .data(d3.range(0, 100, 1))
         .enter()
         .append("rect")
         .attr("x", 0)
         .attr("y", d => d)
-        .attr("width", legendBarWidth) 
+        .attr("width", legendBarWidth)
         .attr("height", 5)
         .attr("fill", d => colorScale(legendScale.invert(d)));
-    
+
     legend.append("g")
-        .attr("transform", `translate(${legendBarWidth + 5}, 0)`) 
+        .attr("transform", `translate(${legendBarWidth + 5}, 0)`)
         .call(legendAxis);
-    
+
     legend.append("text")
         .attr("x", 10)
         .attr("y", -10)
         .attr("font-size", "12px")
         .attr("fill", "black")
         .text("COVID-19 Deaths per 100k");
-    
-    
+
     legend.append("text")
-        .attr("x", legendBarWidth + 10) 
+        .attr("x", legendBarWidth + 10)
         .attr("y", 100)
-        .attr("dy", "0.8em") 
+        .attr("dy", "0.8em")
         .attr("font-size", "10px")
         .attr("fill", "black")
-        .attr("text-anchor", "start") 
+        .attr("text-anchor", "start")
         .text(colorScale.domain()[1]);
 });
