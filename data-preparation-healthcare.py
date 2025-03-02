2901cfdbc3df363f8e1ab095eddb20f604b20fed
MajewskaM <s193360@student.pg.edu.pl>
2025-03-02 21:00:49 +0100

    addition of comments and data summaries

--- data-preparation-healthcare.py
+++ data-preparation-healthcare.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 
+# loading health expenditure data
 health_exp_file = 'datasets/healthcare/healthcare-expenditure-per-capita.txt' 
 health_df = pd.read_csv(health_exp_file, on_bad_lines='skip')
 
-
+# change data format from wide to long format
 health_long = pd.melt(
     health_df,
     id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code"],
@@ -11,20 +13,32 @@ health_long = pd.melt(
     value_name="Health Expenditure"
 )
 
+# extract 4-digit year and convert to integer
+health_long["Year"] = health_long["Year"].str.extract(r"(\d{4})").astype("Int64")
 
-health_long["Year"] = health_long["Year"].str.extract(r"(\d{4})").astype(float).astype("Int64")
-health_long = health_long.dropna(subset=["Health Expenditure", "Year"])
+# data summary
+print(health_long.info())
+print(health_long.head())
+#print(health_long.describe()) # explore health expenditures per capita
+print("Unique Countries:", health_long["Country Name"].nunique())
+print("Year Range:", health_long['Year'].min(), "-", health_long['Year'].max())
+print("Missing Values per Column:")
+print(health_long.isnull().sum())
 
+# remove missing values from 'Health Expenditure' and 'Year'
+health_long = health_long[~np.isnan(health_long["Health Expenditure"])]
+health_long = health_long[~np.isnan(health_long["Year"])]
+
+# convert currency of expenditures from USD to EUR
 usd_to_eur_rate = 0.85
-
 health_long.rename(columns={"Health Expenditure": "Health Expenditure per Capita"}, inplace=True)
 health_long = health_long[["Country Name", "Year", "Health Expenditure per Capita"]]
-health_long["Health Expenditure per Capita"] = health_long["Health Expenditure per Capita"] * usd_to_eur_rate
+health_long["Health Expenditure per Capita"] = np.multiply(health_long["Health Expenditure per Capita"], usd_to_eur_rate)
 
-# print(health_long)
+print("Health expenditures per capita in EUR")
+print(health_long["Health Expenditure per Capita"].describe()) # explore health expenditures in EUR
 
-
-
+# loading and merging life-expectancy&population data for each year
 unique_years = health_long['Year'].unique()
 for year in unique_years:
     life_exp_file = f"datasets/life-expectancy-population/by-years/life-expectancy-population-{year}.csv"
@@ -40,8 +54,15 @@ for year in unique_years:
 
     combined_df = combined_df[combined_df['Total'] == 'total']
     combined_df = combined_df.drop(columns=["Country Name", "Total"])
-    combined_df["Total Health Expenditure"] = combined_df["Health Expenditure per Capita"] * combined_df["Population"]
 
+    # calculating total health expenditure based on per capita and population data
+    combined_df["Total Health Expenditure"] = np.multiply(
+        combined_df["Health Expenditure per Capita"], combined_df["Population"]
+    )
+
+    # filter only data for given 
     yearly_data = combined_df[combined_df['Year'] == year]
+
+    # save each year to separate file
     output_file = f"datasets/healthcare/with-life-expectancy-by-years/life-expectancy-health-expenditure-{year}.csv"
     yearly_data.to_csv(output_file, index=False)
--- data-preparation-life-expectancy-population.py
+++ data-preparation-life-expectancy-population.py
@@ -1,22 +1,6 @@
 import pandas as pd
-import numpy as np
 
-file_path = 'datasets/life-expectancy-population/EU-life-expectancy-population-(1960-2023)-wide.csv'
-df = pd.read_csv(file_path)
-
-df_long = pd.melt(
-    df,
-    id_vars=["Country Name", "Country Code", "Series Name", "Series Code"],
-    var_name="Year",
-    value_name="Value"
-)
-
-df_long["Year"] = df_long["Year"].str.extract(r"(\d{4})").astype(float).astype("Int64")
-df_long = df_long.dropna(subset=["Value", "Year"])
-df_long["Value"] = pd.to_numeric(df_long["Value"], errors="coerce")
-valid_countries = df_long.groupby("Country Name")["Value"].transform("count") > 0
-df_long = df_long[valid_countries]
-
+# function to classify series name - life expectancy or population data
 def classify_series(series_name):
     if pd.isna(series_name):
         return None, None
@@ -24,38 +8,68 @@ def classify_series(series_name):
         return "Life_expectancy", series_name.split(",")[-1].strip()
     elif "Population" in series_name:
         return "Population", series_name.split(",")[-1].strip()
-    else:
-        return None, None
+    
+    return None, None
 
+# loading life-expectancy and population data
+file_path = 'datasets/life-expectancy-population/EU-life-expectancy-population-(1960-2023)-wide.csv'
+df = pd.read_csv(file_path)
+
+# convering from wide to long format
+df_long = pd.melt(
+    df,
+    id_vars=["Country Name", "Country Code", "Series Name", "Series Code"],
+    var_name="Year",
+    value_name="Value"
+)
+
+# extract 4-digit year and convert to integer
+df_long["Year"] = df_long["Year"].str.extract(r"(\d{4})").astype("Int64")
+df_long["Value"] = pd.to_numeric(df_long["Value"], errors="coerce") # change non numeric values to NaN
+
+# data summary
+print(df_long.info())
+print(df_long.head())
+# print(df_long.describe())
+print("Unique Countries:", df_long["Country Name"].nunique())
+print("Year Range:", df_long['Year'].min(), "-", df_long['Year'].max())
+print("Missing Values per Column:")
+print(df_long.isnull().sum())
+print(df_long["Series Name"].unique())
+
+
+# dropping missing values
+df_long = df_long.dropna(subset=["Value", "Year"])
+
+# filter countries with no missing values
+valid_countries = df_long.groupby("Country Name")["Value"].transform("count") > 0
+df_long = df_long[valid_countries]
+
+# by series name extract the type of series and 
+# whether it applies to male, female or total values
 df_long[["Type", "Total"]] = df_long["Series Name"].apply(
     lambda x: pd.Series(classify_series(x))
 )
 
+# remove missing values
 df_long = df_long.dropna(subset=["Type", "Total"])
 
-def standardize_total(total):
-    if 'female' in total:
-        return 'female'
-    elif 'male' in total:
-        return 'male'
-    elif 'total' in total:
-        return 'total'
-    else:
-        return total
-
-df_long["Total"] = df_long["Total"].apply(standardize_total)
+# drop duplicates of data
 df_long = df_long.drop_duplicates(subset=["Country Name", "Year", "Type", "Total"])
 
+# convert type (Life Expectancy, Population) into separate columns 
+# from long to wide
 result = df_long.pivot_table(
     index=["Country Name", "Year", "Total"],
     columns="Type",
     values="Value",
     aggfunc="first"
-).reset_index()
+).reset_index() # preserve index as columns names
 
 result.columns.name = None
 result.rename(columns={"Country Name": "Country"}, inplace=True)
 
+# save data about each year to separate files
 unique_years = result['Year'].unique()
 for year in unique_years:
     # we dont have all data for 2023
@@ -64,10 +78,10 @@ for year in unique_years:
         output_file = f"datasets/life-expectancy-population/by-years/life-expectancy-population-{year}.csv"
         yearly_data.to_csv(output_file, index=False)
 
-# for andora mocano san marino we dont have life-expectancy
+# for andora, mocano, san marino we dont have life-expectancy
+# save processed data, used to join other datasets
 output_file_path = 'datasets/life-expectancy-population/EU-life-expectancy-population-(1960-2023).csv'
 result.to_csv(output_file_path, index=False)
 
-distinct_countries = result['Country'].unique()
-print(distinct_countries)
+
 
